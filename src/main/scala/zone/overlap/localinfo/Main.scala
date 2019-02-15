c09726301201782ecaf1bbbb4d7c67d1b50c233c
Vy-Shane Xie <shane@node.mu>
2019-02-15 23:45:58 +0800

    Clean up Main

--- src/main/scala/zone/overlap/localinfo/Main.scala
+++ src/main/scala/zone/overlap/localinfo/Main.scala
@@ -36,21 +36,21 @@ object Main extends App with LazyLogging {
         .bind[GetLocalInfoRpc].toInstance(new GetLocalInfoRpc(NoCache))
     }
 
-    // Startup
+    // Start application
     design.withProductionMode.noLifeCycleLogging
       .withSession(_.build[Application].run())
+  }
 
-    lazy val cachedWeatherRepositoryProvider: FDBDatabase => CachedWeatherRepository = { db =>
-      new CachedWeatherRepository(db, config.fdbKeySpaceDirectory)
-    }
+  private val cachedWeatherRepositoryProvider: (FDBDatabase, Config) => CachedWeatherRepository = { (db, config) =>
+    new CachedWeatherRepository(db, config.fdbKeySpaceDirectory)
+  }
 
-    lazy val foundationDbCacheProvider: CachedWeatherRepository => FoundationDbCache = { repository =>
-      val purgeSignal = Observable.interval(1 minute).map(_ => ())
-      FoundationDbCache(repository, purgeSignal, Clock.systemUTC(), 30 minutes)
-    }
+  private val foundationDbCacheProvider: CachedWeatherRepository => FoundationDbCache = { repository =>
+    val purgeSignal = Observable.interval(1 minute).map(_ => ())
+    FoundationDbCache(repository, purgeSignal, Clock.systemUTC(), 30 minutes)
+  }
 
-    lazy val cachedGetLocalInfoRpcProvider: FoundationDbCache => GetLocalInfoRpc = { foundationDbCache =>
-      new GetLocalInfoRpc(foundationDbCache)
-    }
+  private val cachedGetLocalInfoRpcProvider: FoundationDbCache => GetLocalInfoRpc = { foundationDbCache =>
+    new GetLocalInfoRpc(foundationDbCache)
   }
 }
