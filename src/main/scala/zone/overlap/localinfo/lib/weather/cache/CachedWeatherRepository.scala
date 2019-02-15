01524f16d51bbce75c7b747f49b1a4264d7b9faa
Vy-Shane Xie <shane@node.mu>
2019-02-15 20:57:21 +0800

    Implement deletion of cached weather records

--- src/main/scala/zone/overlap/localinfo/lib/weather/cache/CachedWeatherRepository.scala
+++ src/main/scala/zone/overlap/localinfo/lib/weather/cache/CachedWeatherRepository.scala
@@ -7,8 +7,10 @@ import java.time.Instant
 import com.apple.foundationdb.record.RecordMetaData
 import com.apple.foundationdb.record.metadata.Index
 import com.apple.foundationdb.record.metadata.Key.Expressions._
-import com.apple.foundationdb.record.provider.foundationdb.{FDBDatabase, FDBRecordContext, FDBRecordStore, FDBStoredRecord}
+import com.apple.foundationdb.record.provider.foundationdb._
 import com.apple.foundationdb.record.provider.foundationdb.keyspace.{KeySpace, KeySpaceDirectory}
+import com.apple.foundationdb.record.query.RecordQuery
+import com.apple.foundationdb.record.query.expressions.Query
 import com.apple.foundationdb.tuple.Tuple
 import com.google.protobuf.Message
 import monix.eval.Task
@@ -19,12 +21,11 @@ class CachedWeatherRepository(db: FDBDatabase, keySpaceDirectoryName: String) {
 
   private val recordMetaData = {
     val metaDataBuilder = RecordMetaData.newBuilder().setRecords(CachedWeatherProto.getDescriptor)
-    metaDataBuilder.addIndex("CachedWeather",
-      new Index(
-        "retrieved_at_index",
-        field("retrieved_at").nest(concat(field("seconds"), field("nanos")))
-      )
+    val retrievedAtIndex = new Index(
+      "retrieved_at_index",
+      field("retrieved_at").nest("seconds")
     )
+    metaDataBuilder.addIndex("CachedWeather", retrievedAtIndex)
     metaDataBuilder.build()
   }
 
@@ -45,24 +46,43 @@ class CachedWeatherRepository(db: FDBDatabase, keySpaceDirectoryName: String) {
 
   def get(localityKey: String): Task[Option[CachedWeather]] = {
     Task {
-      Option[FDBStoredRecord[Message]] {
-        db.run(context => {
-          getRecordStore(context).loadRecord(Tuple.from(localityKey))
-        })
-      } map toCachedWeather
+      Option[FDBStoredRecord[Message]](
+        db.run(
+          getRecordStore(_).loadRecord(Tuple.from(localityKey))
+        ))
+        .map(toCachedWeather(_))
     }
   }
 
   def save(cachedWeather: CachedWeather): Task[Unit] = {
     Task[FDBStoredRecord[Message]] {
-      db.run(context => {
-        getRecordStore(context).saveRecord(CachedWeather.toJavaProto(cachedWeather))
-      })
+      db.run(
+        getRecordStore(_).saveRecord(CachedWeather.toJavaProto(cachedWeather))
+      )
     } map (_ => ())
   }
 
-  def delete(locationKey: String, olderThan: Instant): Task[Boolean] = {
-    ???
+  def deleteOlderThan(instant: Instant): Task[Unit] = {
+    Task {
+      db.run { context =>
+        val store = getRecordStore(context)
+
+        val query = RecordQuery
+          .newBuilder()
+          .setRecordType("CachedWeather")
+          .setFilter(Query.field("retrieved_at") matches {
+            Query.field("seconds") lessThan instant.getEpochSecond
+          })
+          .build()
+
+        store
+          .executeQuery(query)
+          .forEach(r => store.deleteRecord(r.getPrimaryKey))
+          .join()
+
+        null
+      }
+    }
   }
 
   private def toCachedWeather(storedRecord: FDBStoredRecord[Message]): CachedWeather = {
--- src/test/scala/zone/overlap/localinfo/lib/weather/cache/CachedWeatherRepositorySpec.scala
+++ src/test/scala/zone/overlap/localinfo/lib/weather/cache/CachedWeatherRepositorySpec.scala
@@ -2,17 +2,31 @@
 
 package zone.overlap.localinfo.lib.weather.cache
 
-import org.scalatest.{AsyncWordSpec, Matchers}
+import java.time.Instant
+
+import com.google.protobuf.timestamp.Timestamp
+import org.scalatest.{AsyncWordSpec, BeforeAndAfterEach, Matchers}
 import monix.execution.Scheduler.Implicits.global
 import zone.overlap.localinfo.util.Fakes._
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 /*
  * Integration tests for the CacheWeatherRepository
  * The tests in this spec need a Docker engine to run FoundationDB
  */
-class CachedWeatherRepositorySpec extends AsyncWordSpec with Matchers with FoundationDbDockerTestKit {
+class CachedWeatherRepositorySpec
+    extends AsyncWordSpec
+    with Matchers
+    with BeforeAndAfterEach
+    with FoundationDbDockerTestKit {
 
   val repository = new CachedWeatherRepository(fdb, "local-info-tests")
+
+  override def beforeEach(): Unit = {
+    Await.result(repository.deleteOlderThan(Instant.now()).runAsync, 5 seconds)
+  }
 
   "CachedWeatherRepository" when {
     "asked to get a record that doesn't exist" should {
@@ -30,17 +44,35 @@ class CachedWeatherRepositorySpec extends AsyncWordSpec with Matchers with Found
         retrieved.runAsync.map(_ shouldEqual Option(cachedWeather))
       }
     }
-    "asked to save a record with a duplicate locality_key" should {
-      "raise an error" in (pending)
-    }
-    "asked to delete a record doesn't exist" should {
-      "return false" in (pending)
-    }
-    "asked to delete a record that is newer than the olderThan parameter" should {
-      "not delete the record and return false" in (pending)
-    }
-    "asked to delete a record that is older than the olderThan parameter" should {
-      "delete the record and return true" in (pending)
+    "asked to delete records older than an instant" should {
+      "delete records older than the instant" in {
+        val now = Instant.now()
+        val cachedWeather = randomCachedWeather().withRetrievedAt(Timestamp(now.getEpochSecond))
+        val result = for {
+          _ <- repository.save(cachedWeather)
+          found <- repository.get(cachedWeather.localityKey)
+          _ <- repository.deleteOlderThan(now.plusSeconds(10))
+          notFound <- repository.get(cachedWeather.localityKey)
+        } yield (found, notFound)
+        result.runAsync.map { r =>
+          r._1 shouldEqual Option(cachedWeather)
+          r._2 shouldBe None
+        }
+      }
+      "not delete records created more recently than the instant" in {
+        val now = Instant.now()
+        val cachedWeather = randomCachedWeather().withRetrievedAt(Timestamp(now.getEpochSecond))
+        val result = for {
+          _ <- repository.save(cachedWeather)
+          found <- repository.get(cachedWeather.localityKey)
+          _ <- repository.deleteOlderThan(now.minusSeconds(10))
+          stillFound <- repository.get(cachedWeather.localityKey)
+        } yield (found, stillFound)
+        result.runAsync.map { r =>
+          r._1 shouldEqual Option(cachedWeather)
+          r._2 shouldEqual Option(cachedWeather)
+        }
+      }
     }
   }
 }
