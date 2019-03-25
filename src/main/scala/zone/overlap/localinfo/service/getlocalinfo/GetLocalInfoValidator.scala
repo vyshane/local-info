546f2ed93f41fe31f533ef6c788b5fc12c5b8e3d
Vy-Shane Xie <shane@node.mu>
2019-03-25 23:40:08 +0800

    Fix validation logic

--- src/main/scala/zone/overlap/localinfo/service/getlocalinfo/GetLocalInfoValidator.scala
+++ src/main/scala/zone/overlap/localinfo/service/getlocalinfo/GetLocalInfoValidator.scala
@@ -19,16 +19,23 @@ object GetLocalInfoValidator {
 
   private def validateCoordinate(coordinate: Option[Coordinate]): ValidationResult[Coordinate] = {
     coordinate match {
-      case None => CoordinateIsRequired.invalidNec
-      case Some(c) =>
-        if (c.longitude < -180 || c.longitude > 180) LongitudeOutOfRange.invalidNec
-        else if (c.latitude < 0 || c.latitude > 90) LatitudeOutOfRange.invalidNec
-        else c.validNec
+      case None    => CoordinateIsRequired.invalidNec
+      case Some(c) => (validateLatitude(c.latitude), validateLongitude(c.longitude)).mapN((_, _) => c)
     }
   }
 
   private def validateZoomLevel(zoomLevel: ZoomLevel): ValidationResult[ZoomLevel] = {
-    if (zoomLevel != ZOOM_LEVEL_UNSPECIFIED) ZoomLevelIsRequired.invalidNec
+    if (zoomLevel == ZOOM_LEVEL_UNSPECIFIED) ZoomLevelIsRequired.invalidNec
     else zoomLevel.validNec
+  }
+
+  private def validateLatitude(latitude: Double): ValidationResult[Double] = {
+    if (latitude < -90 || latitude > 90) LatitudeOutOfRange.invalidNec
+    else latitude.validNec
+  }
+
+  private def validateLongitude(longitude: Double): ValidationResult[Double] = {
+    if (longitude < -180 || longitude > 180) LongitudeOutOfRange.invalidNec
+    else longitude.validNec
   }
 }
